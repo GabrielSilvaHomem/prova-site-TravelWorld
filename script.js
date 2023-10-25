3dd6cff9ebb4c05e463613d51a1d1ea57bd6344a
gabriel_homem <hsleirbag@hotmail.com>
2023-10-24 21:34:30 -0300

    concluido

---
+++ script.js
@@ -0,0 +1,10 @@
+function aumentarImagem(id) {
+  var imagem = document.getElementById(id);
+  imagem.style.width = "75vw";
+  imagem.style.height = "75vh";
+}
+function diminuirImagem(id) {
+  var imagem = document.getElementById(id);
+  imagem.style.width = "300px";
+  imagem.style.height = "300px";
+}
